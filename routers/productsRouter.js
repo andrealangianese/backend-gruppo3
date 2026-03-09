c61d2ab84befde5847e4bca729d670c84bbef3dc
andrealangianese <andre.langia.01@gmail.com>
2026-03-09 14:39:15 +0100

    feat: completo router con rotte index e show

---
+++ routers/productsRouter.js
@@ -0,0 +1,22 @@
+// import express e router
+
+const express = require('express');
+
+const router = express.Router();
+ 
+// import controller da usare 
+
+const productsController = require('../controllers/productsController')
+
+// setto rotte
+
+// rotta index
+
+router.get('/', productsController.index)
+
+// rotta show
+
+router.get('/:id', productsController.show)
+
+module.exports = router
+
