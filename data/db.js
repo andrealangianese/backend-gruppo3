dc8cddd4cb8a55e59addfd38b6fda5e10c8b5a7b
andrealangianese <andre.langia.01@gmail.com>
2026-03-21 11:31:07 +0100

    fix:db connection

--- data/db.js
+++ data/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 14798, // Porta di Aiven
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
