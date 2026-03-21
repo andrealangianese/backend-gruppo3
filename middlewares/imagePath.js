3edbbcd0c715fe3a0d4c70c00292bfa19e6d9e0a
andrealangianese <andre.langia.01@gmail.com>
2026-03-21 17:43:25 +0100

    fix: forzo https nell'image path

--- middlewares/imagePath.js
+++ middlewares/imagePath.js
@@ -1,6 +1,6 @@
 function imagePath(req, res, next) {
     // creo nuova proprietà da aggiungere a req per path img
-    req.imagePath = `${req.protocol}://${req.get('host')}/image/`;
+    req.imagePath = `https://${req.get('host')}/image/`;
     // procedi con la risposta
     next();
 }
